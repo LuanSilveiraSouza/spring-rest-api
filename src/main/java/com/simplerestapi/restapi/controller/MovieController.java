5c2057c5158147770d7c39ddc3c7a47608d443e4
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2020-06-27 18:35:45 -0300

    Added http status codes

--- src/main/java/com/simplerestapi/restapi/controller/MovieController.java
+++ src/main/java/com/simplerestapi/restapi/controller/MovieController.java
@@ -4,6 +4,8 @@ import com.simplerestapi.restapi.model.Movie;
 import com.simplerestapi.restapi.repository.MovieRepository;
 import com.simplerestapi.restapi.services.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,12 +24,14 @@ public class MovieController {
     }
 
     @GetMapping("/movie/{id}")
-    public Movie getMovie(@PathVariable("id") Long id) {
+    public ResponseEntity<Movie> getMovie(@PathVariable("id") Long id) {
+        final Movie movie = movieService.findById(id);
 
-        return movieService.findById(id);
+        return ResponseEntity.ok(movie);
     }
 
     @PostMapping("/movie")
+    @ResponseStatus(HttpStatus.CREATED)
     public String createMovie(@Valid @RequestBody Movie movie) {
         try {
             movieService.create(movie);
